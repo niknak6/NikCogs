17bb2f472c491bd265757ea43566416945bbf278
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-22 19:09:16 -0400

    Update pinextender.py

--- pinextender/pinextender.py
+++ pinextender/pinextender.py
@@ -68,6 +68,9 @@ class PinExtender(commands.Cog):
             # Get the built-in pin confirmation message object by fetching the latest system message in the channel
             pin_confirmation_message = await channel.fetch_message(channel.last_message_id)
 
+            # React to the built-in pin confirmation message with a :pushpin: emoji to indicate that it was added to the extended pins message
+            await pin_confirmation_message.add_reaction("\U0001F4CC")
+
             # React to each message link in the extended pins message with a :wastebasket: emoji to allow unpinning them later
             await extended_pins_message.add_reaction("\U0001F5D1")
 
