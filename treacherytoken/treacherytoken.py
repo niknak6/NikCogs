22a6c68c283fa1e19bc309da07d03f599062e7f6
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-11-03 22:33:08 -0400

    Update treacherytoken.py

--- treacherytoken/treacherytoken.py
+++ treacherytoken/treacherytoken.py
@@ -33,7 +33,7 @@ class TreacheryToken(commands.Cog):
         # Create the embed message
         embed = discord.Embed(title=":coin: WoW Token Price :coin:", color=0x00ff00)
         embed.add_field(name="Current Price", value=f"{price} gold")
-        embed.set_footer(text=f"Last updated at {timestamp}") # This is the change I made to use the dynamic timestamp in the embed footer
+        embed.add_field(name="Last updated", value=timestamp) # This is the change I made to move the dynamic timestamp from the embed footer to another field in the embed
 
         # Send the embed message
         await ctx.send(embed=embed)
