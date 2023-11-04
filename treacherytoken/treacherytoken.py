915f6e4c149a1788164fa032a2f9fc7a944ad82c
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-11-03 21:09:18 -0400

    Update treacherytoken.py

--- treacherytoken/treacherytoken.py
+++ treacherytoken/treacherytoken.py
@@ -1,6 +1,9 @@
+# treacherytoken.py
+# A redbot 3.5 cog that parses information from wowtokenprices.com and returns the price of a wow token.
+
 import discord
 from redbot.core import commands
-from requests_html import HTMLSession
+import requests
 from bs4 import BeautifulSoup
 
 class TreacheryToken(commands.Cog):
@@ -14,30 +17,18 @@ class TreacheryToken(commands.Cog):
         """Shows the current price of a wow token in US dollars."""
         # Send a message to the context channel saying "Loading WoW Token Information..."
         loading_message = await ctx.send("Loading WoW Token Information...")
-        # Create a HTMLSession object
-        session = HTMLSession()
-        # Get the response from the website
-        response = session.get("https://wowtokenprices.com/")
-        # Render the JavaScript and HTML content
-        response.html.render()
+        # Get the HTML content from wowtokenprices.com
+        response = requests.get("https://wowtokenprices.com/")
         # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.html.html, "html.parser")
+        soup = BeautifulSoup(response.content, "html.parser")
         # Find the span element with id="us-money-text" and get its text
         price = soup.find("span", id="us-money-text").text
         # Remove the comma and the dollar sign from the price and convert it to an integer
         price = int(price.replace(",", "").replace("$", ""))
         # Format the price with commas
         price = "{:,}".format(price)
-        # Find the p element with id="us-datetime" and get its text
-        time = soup.find("p", id="us-datetime").text
-        # Format the time as you like
-        time = f"Last updated at {time}"
         # Create an embed with the price and a gold coin emoji
         embed = discord.Embed(title=":coin: WoW Token Price :coin:", color=0xffd700)
         embed.add_field(name="US Region", value=price)
-        # Add another field to the embed with the time and a clock emoji
-        embed.add_field(name="Update Time", value=f":clock1: {time}")
         # Edit the loading message with the embed
         await loading_message.edit(content=None, embed=embed)
-        # Close the session
-        session.close()
