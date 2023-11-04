7d79fc742858b6d70176de366135af4e10ff59a7
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-11-03 20:04:51 -0400

    Update treacherytoken.py

--- treacherytoken/treacherytoken.py
+++ treacherytoken/treacherytoken.py
@@ -1,7 +1,13 @@
+# treacherytoken.py
+# A redbot 3.5 cog that parses information from wowtokenprices.com and returns the price of a wow token.
+
 import discord
 from redbot.core import commands
 import requests
 from bs4 import BeautifulSoup
+import time # Added for the try-except block
+from urllib3.util.retry import Retry # Added for the retry class
+from requests.adapters import HTTPAdapter # Added for the HTTPAdapter
 
 class TreacheryToken(commands.Cog):
     """A cog that shows the current price of a wow token."""
@@ -14,8 +20,21 @@ class TreacheryToken(commands.Cog):
         """Shows the current price of a wow token in US dollars."""
         # Send a message to the context channel saying "Loading WoW Token Information..."
         loading_message = await ctx.send("Loading WoW Token Information...")
-        # Get the HTML content from wowtokenprices.com
-        response = requests.get("https://wowtokenprices.com/")
+        # Create a Session object and mount the adapter with the retry object
+        session = requests.Session()
+        retry = Retry(total=3, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504], method_whitelist=["GET"])
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount("https://", adapter)
+        # Use a try-except block to catch the exception and retry the request after a delay
+        try:
+            # Get the HTML content from wowtokenprices.com using the session object
+            response = session.get("https://wowtokenprices.com/")
+        except requests.exceptions.ConnectionError as e:
+            print("Connection error: {}".format(e))
+            print("Retrying after 5 seconds...")
+            time.sleep(5)
+            # Recursively retry the request
+            return await self.wowtoken(ctx)
         # Parse the HTML content using BeautifulSoup
         soup = BeautifulSoup(response.content, "html.parser")
         # Find the span element with id="us-money-text" and get its text
@@ -30,6 +49,6 @@ class TreacheryToken(commands.Cog):
         embed = discord.Embed(title=":coin: WoW Token Price :coin:", color=0xffd700)
         embed.add_field(name="US Region", value=price)
         # Set the footer with the last change time
-        embed.set_footer(text=last_change)
+        embed.set_footer(text=last_change) # Removed the icon_url argument
         # Edit the loading message with the embed
         await loading_message.edit(content=None, embed=embed)
