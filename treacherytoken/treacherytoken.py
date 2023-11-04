f90d702a0b40c857cade18c57aac982659853bc9
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-11-03 21:01:05 -0400

    Update treacherytoken.py

--- treacherytoken/treacherytoken.py
+++ treacherytoken/treacherytoken.py
@@ -1,6 +1,7 @@
 import discord
 from redbot.core import commands
-import requests
+import selenium
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
 class TreacheryToken(commands.Cog):
@@ -14,18 +15,26 @@ class TreacheryToken(commands.Cog):
         """Shows the current price of a wow token in US dollars."""
         # Send a message to the context channel saying "Loading WoW Token Information..."
         loading_message = await ctx.send("Loading WoW Token Information...")
-        # Get the HTML content from wowtokenprices.com
-        response = requests.get("https://wowtokenprices.com/")
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.content, "html.parser")
+        # Create a webdriver object that can control Chrome
+        driver = webdriver.Chrome()
+        # Open the website
+        driver.get("https://wowtokenprices.com/")
+        # Wait for the page to load
+        driver.implicitly_wait(10)
+        # Get the HTML source code
+        html = driver.page_source
+        # Close the browser
+        driver.quit()
+        # Parse the HTML source code using BeautifulSoup
+        soup = BeautifulSoup(html, "html.parser")
         # Find the span element with id="us-money-text" and get its text
         price = soup.find("span", id="us-money-text").text
         # Remove the comma and the dollar sign from the price and convert it to an integer
         price = int(price.replace(",", "").replace("$", ""))
         # Format the price with commas
         price = "{:,}".format(price)
-        # Find the p element with id="us-datetime" using its CSS selector and get its text
-        time = soup.select_one("p#region-date").text
+        # Find the p element with id="us-datetime" and get its text
+        time = soup.find("p", id="us-datetime").text
         # Format the time as you like
         time = f"Last updated at {time}"
         # Create an embed with the price and a gold coin emoji
