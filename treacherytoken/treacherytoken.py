bba046518e52809726b01f37f3634e0dbd572a50
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-11-04 21:30:42 -0400

    Update treacherytoken.py

--- treacherytoken/treacherytoken.py
+++ treacherytoken/treacherytoken.py
@@ -1,11 +1,12 @@
 # Import the necessary modules
 import discord
 from redbot.core import commands
-import requests
+import aiohttp # Use aiohttp instead of requests
+import humanize # Use humanize to format the values and the timestamp
 
 # Define the cog class
 class TreacheryToken(commands.Cog):
-    """A cog that shows the WoW token price"""
+    """A cog that shows the WoW token price in a specified region"""
 
     def __init__(self, bot):
         self.bot = bot
@@ -23,8 +24,9 @@ class TreacheryToken(commands.Cog):
 
         # Get the json data from the website
         url = "https://wowtokenprices.com/current_prices.json"
-        response = requests.get(url)
-        data = response.json()
+        async with aiohttp.ClientSession() as session: # Use async with to create a session and get the response
+            async with session.get(url) as response:
+                data = await response.json() # Use await to get the json data
 
         # Extract the relevant information
         price = data[region.lower()]["current_price"]
@@ -38,46 +40,74 @@ class TreacheryToken(commands.Cog):
         thirty_day_high = data[region.lower()]["30_day_high"]
 
         # Convert the values to integers
-        price = int(price)
-        one_day_low = int(one_day_low)
-        one_day_high = int(one_day_high)
-        seven_day_low = int(seven_day_low)
-        seven_day_high = int(seven_day_high)
-        thirty_day_low = int(thirty_day_low)
-        thirty_day_high = int(thirty_day_high)
+        try:
+            price = int(price)
+        except ValueError:
+            price = "No Data"
+        try:
+            one_day_low = int(one_day_low)
+        except ValueError:
+            one_day_low = "No Data"
+        try:
+            one_day_high = int(one_day_high)
+        except ValueError:
+            one_day_high = "No Data"
+        try:
+            seven_day_low = int(seven_day_low)
+        except ValueError:
+            seven_day_low = "No Data"
+        try:
+            seven_day_high = int(seven_day_high)
+        except ValueError:
+            seven_day_high = "No Data"
+        try:
+            thirty_day_low = int(thirty_day_low)
+        except ValueError:
+            thirty_day_low = "No Data"
+        try:
+            thirty_day_high = int(thirty_day_high)
+        except ValueError:
+            thirty_day_high = "No Data"
 
         # Format the values with commas
-        price = f"{price:,}"
-        one_day_low = f"{one_day_low:,}"
-        one_day_high = f"{one_day_high:,}"
-        seven_day_low = f"{seven_day_low:,}"
-        seven_day_high = f"{seven_day_high:,}"
-        thirty_day_low = f"{thirty_day_low:,}"
-        thirty_day_high = f"{thirty_day_high:,}"
+        if price != "No Data":
+            price = humanize.intcomma(price) # Use humanize.intcomma to add commas to the integers
+        if one_day_low != "No Data":
+            one_day_low = humanize.intcomma(one_day_low)
+        if one_day_high != "No Data":
+            one_day_high = humanize.intcomma(one_day_high)
+        if seven_day_low != "No Data":
+            seven_day_low = humanize.intcomma(seven_day_low)
+        if seven_day_high != "No Data":
+            seven_day_high = humanize.intcomma(seven_day_high)
+        if thirty_day_low != "No Data":
+            thirty_day_low = humanize.intcomma(thirty_day_low)
+        if thirty_day_high != "No Data":
+            thirty_day_high = humanize.intcomma(thirty_day_high)
 
-        # Create the dynamic timestamp syntax
-        timestamp = f"<t:{time}:f>"
+        # Format the timestamp with a human-readable format
+        timestamp = humanize.naturaltime(time) # Use humanize.naturaltime to convert the unix epoch time to a relative time
 
         # Create the embed message
         embed = discord.Embed(title=f":coin: WoW Token Price in {region.upper()} :coin:", color=0x00ff00)
         change_emoji = "📈" if change > 0 else "📉" # This is the emoji for the last change
-        embed.add_field(name="", value=f"Current Price: {price} ({change_emoji} {change})") # This is the merged field with the emoji
-        embed.add_field(name="", value=f"Updated {timestamp}") # This is the field without the small text
-        embed.add_field(name="", value="\n", inline=False) # This is the line break using the newline character
+        embed.add_field(name=discord.Embed.Empty, value=f"Current Price: {price} ({change_emoji} {change})") # This is the merged field with the emoji
+        embed.add_field(name=discord.Embed.Empty, value=f"Updated {timestamp}") # This is the field without the small text
+        embed.add_field(name=discord.Embed.Empty, value="\n", inline=False) # This is the line break using the newline character
         one_day_low_emoji = "📈" if price > one_day_low else "📉" # This is the emoji for the 1 day low
-        embed.add_field(name="", value=f"1 Day Low {one_day_low_emoji}: {one_day_low}", inline=True) # This is the field with the emoji
+        embed.add_field(name=discord.Embed.Empty, value=f"1 Day Low {one_day_low_emoji}: {one_day_low}", inline=True) # This is the field with the emoji
         one_day_high_emoji = "📈" if price < one_day_high else "📉" # This is the emoji for the 1 day high
-        embed.add_field(name="", value=f"1 Day High {one_day_high_emoji}: {one_day_high}", inline=True) # This is the field with the emoji
-        embed.add_field(name="", value="\n", inline=False) # This is the line break using the newline character
+        embed.add_field(name=discord.Embed.Empty, value=f"1 Day High {one_day_high_emoji}: {one_day_high}", inline=True) # This is the field with the emoji
+        embed.add_field(name=discord.Embed.Empty, value="\n", inline=False) # This is the line break using the newline character
         seven_day_low_emoji = "📈" if price > seven_day_low else "📉" # This is the emoji for the 7 day low
-        embed.add_field(name="", value=f"7 Day Low {seven_day_low_emoji}: {seven_day_low}", inline=True) # This is the field with the emoji
+        embed.add_field(name=discord.Embed.Empty, value=f"7 Day Low {seven_day_low_emoji}: {seven_day_low}", inline=True) # This is the field with the emoji
         seven_day_high_emoji = "📈" if price < seven_day_high else "📉" # This is the emoji for the 7 day high
-        embed.add_field(name="", value=f"7 Day High {seven_day_high_emoji}: {seven_day_high}", inline=True) # This is the field with the emoji
-        embed.add_field(name="", value="\n", inline=False) # This is the line break using the newline character
+        embed.add_field(name=discord.Embed.Empty, value=f"7 Day High {seven_day_high_emoji}: {seven_day_high}", inline=True) # This is the field with the emoji
+        embed.add_field(name=discord.Embed.Empty, value="\n", inline=False) # This is the line break using the newline character
         thirty_day_low_emoji = "📈" if price > thirty_day_low else "📉" # This is the emoji for the 30 day low
-        embed.add_field(name="", value=f"30 Day Low {thirty_day_low_emoji}: {thirty_day_low}", inline=True) # This is the field with the emoji
+        embed.add_field(name=discord.Embed.Empty, value=f"30 Day Low {thirty_day_low_emoji}: {thirty_day_low}", inline=True) # This is the field with the emoji
         thirty_day_high_emoji = "📈" if price < thirty_day_high else "📉" # This is the emoji for the 30 day high
-        embed.add_field(name="", value=f"30 Day High {thirty_day_high_emoji}: {thirty_day_high}", inline=True) # This is the field with the emoji
+        embed.add_field(name=discord.Embed.Empty, value=f"30 Day High {thirty_day_high_emoji}: {thirty_day_high}", inline=True) # This is the field with the emoji
 
         # Send the embed message
         await ctx.send(embed=embed)
