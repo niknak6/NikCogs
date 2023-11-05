d9f1bada0c6b5e2cf5c777e20dc6dc1bc4b39164
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-11-04 21:20:38 -0400

    Update treacherytoken.py

--- treacherytoken/treacherytoken.py
+++ treacherytoken/treacherytoken.py
@@ -12,8 +12,14 @@ class TreacheryToken(commands.Cog):
 
     # Define the command
     @commands.command()
-    async def wowtoken(self, ctx):
-        """Shows the WoW token price in US region"""
+    async def wowtoken(self, ctx, region: str = "us"):
+        """Shows the WoW token price in a specified region"""
+
+        # Validate the region argument
+        valid_regions = ["us", "eu", "cn", "tw", "kr"]
+        if region.lower() not in valid_regions:
+            await ctx.send(f"Invalid region: {region}. Please choose one of these: {', '.join(valid_regions)}.")
+            return
 
         # Get the json data from the website
         url = "https://wowtokenprices.com/current_prices.json"
@@ -21,15 +27,24 @@ class TreacheryToken(commands.Cog):
         data = response.json()
 
         # Extract the relevant information
-        price = data["us"]["current_price"]
-        time = data["us"]["time_of_last_change_unix_epoch"]
-        change = data["us"]["last_change"] # This is the line that passes the integer as a raw number
-        one_day_low = data["us"]["1_day_low"]
-        one_day_high = data["us"]["1_day_high"]
-        seven_day_low = data["us"]["7_day_low"]
-        seven_day_high = data["us"]["7_day_high"]
-        thirty_day_low = data["us"]["30_day_low"]
-        thirty_day_high = data["us"]["30_day_high"]
+        price = data[region.lower()]["current_price"]
+        time = data[region.lower()]["time_of_last_change_unix_epoch"]
+        change = int(data[region.lower()]["last_change"]) # This is the line that converts the string to an integer
+        one_day_low = data[region.lower()]["1_day_low"]
+        one_day_high = data[region.lower()]["1_day_high"]
+        seven_day_low = data[region.lower()]["7_day_low"]
+        seven_day_high = data[region.lower()]["7_day_high"]
+        thirty_day_low = data[region.lower()]["30_day_low"]
+        thirty_day_high = data[region.lower()]["30_day_high"]
+
+        # Convert the values to integers
+        price = int(price)
+        one_day_low = int(one_day_low)
+        one_day_high = int(one_day_high)
+        seven_day_low = int(seven_day_low)
+        seven_day_high = int(seven_day_high)
+        thirty_day_low = int(thirty_day_low)
+        thirty_day_high = int(thirty_day_high)
 
         # Format the values with commas
         price = f"{price:,}"
@@ -44,24 +59,24 @@ class TreacheryToken(commands.Cog):
         timestamp = f"<t:{time}:f>"
 
         # Create the embed message
-        embed = discord.Embed(title=":coin: WoW Token Price :coin:", color=0x00ff00)
+        embed = discord.Embed(title=f":coin: WoW Token Price in {region.upper()} :coin:", color=0x00ff00)
         change_emoji = "📈" if change > 0 else "📉" # This is the emoji for the last change
         embed.add_field(name="", value=f"Current Price: {price} ({change_emoji} {change})") # This is the merged field with the emoji
         embed.add_field(name="", value=f"Updated {timestamp}") # This is the field without the small text
         embed.add_field(name="", value="\n", inline=False) # This is the line break using the newline character
-        one_day_low_emoji = "📈" if one_day_low > price else "📉" # This is the emoji for the 1 day low
+        one_day_low_emoji = "📈" if price > one_day_low else "📉" # This is the emoji for the 1 day low
         embed.add_field(name="", value=f"1 Day Low {one_day_low_emoji}: {one_day_low}", inline=True) # This is the field with the emoji
-        one_day_high_emoji = "📈" if one_day_high > price else "📉" # This is the emoji for the 1 day high
+        one_day_high_emoji = "📈" if price < one_day_high else "📉" # This is the emoji for the 1 day high
         embed.add_field(name="", value=f"1 Day High {one_day_high_emoji}: {one_day_high}", inline=True) # This is the field with the emoji
         embed.add_field(name="", value="\n", inline=False) # This is the line break using the newline character
-        seven_day_low_emoji = "📈" if seven_day_low > price else "📉" # This is the emoji for the 7 day low
+        seven_day_low_emoji = "📈" if price > seven_day_low else "📉" # This is the emoji for the 7 day low
         embed.add_field(name="", value=f"7 Day Low {seven_day_low_emoji}: {seven_day_low}", inline=True) # This is the field with the emoji
-        seven_day_high_emoji = "📈" if seven_day_high > price else "📉" # This is the emoji for the 7 day high
+        seven_day_high_emoji = "📈" if price < seven_day_high else "📉" # This is the emoji for the 7 day high
         embed.add_field(name="", value=f"7 Day High {seven_day_high_emoji}: {seven_day_high}", inline=True) # This is the field with the emoji
         embed.add_field(name="", value="\n", inline=False) # This is the line break using the newline character
-        thirty_day_low_emoji = "📈" if thirty_day_low > price else "📉" # This is the emoji for the 30 day low
+        thirty_day_low_emoji = "📈" if price > thirty_day_low else "📉" # This is the emoji for the 30 day low
         embed.add_field(name="", value=f"30 Day Low {thirty_day_low_emoji}: {thirty_day_low}", inline=True) # This is the field with the emoji
-        thirty_day_high_emoji = "📈" if thirty_day_high > price else "📉" # This is the emoji for the 30 day high
+        thirty_day_high_emoji = "📈" if price < thirty_day_high else "📉" # This is the emoji for the 30 day high
         embed.add_field(name="", value=f"30 Day High {thirty_day_high_emoji}: {thirty_day_high}", inline=True) # This is the field with the emoji
 
         # Send the embed message
