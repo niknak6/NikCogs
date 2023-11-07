8f9f418ffb71f5a54826af17a6e052271e9b9316
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-11-06 22:22:07 -0500

    Update treacherytoken.py

--- treacherytoken/treacherytoken.py
+++ treacherytoken/treacherytoken.py
@@ -1,8 +1,12 @@
+# Import the time module
+import time
+
+# Import the other modules
 from redbot.core import commands
 import requests
 import pandas as pd
 import discord
-from datetime import datetime, timedelta # CHANGE: added timedelta
+from datetime import datetime, timedelta
 
 class TreacheryToken(commands.Cog):
     """A cog that shows the price of the wow token"""
@@ -13,10 +17,22 @@ class TreacheryToken(commands.Cog):
     @commands.command()
     async def wowtoken(self, ctx):
         """Shows the current, weekly, monthly, 6 month and 1 year high and low price of the wow token in US region"""
+        # Get the start time of the whole code
+        start_time = time.time()
+
         # Get the json data from the url
         url = "https://data.wowtoken.app/token/history/us/1y.json"
         response = requests.get(url)
         data = response.json()
+
+        # Get the end time of getting the json data
+        end_time = time.time()
+
+        # Calculate the duration of getting the json data
+        duration = end_time - start_time
+
+        # Print the duration of getting the json data
+        print(f"Getting the json data took {duration} seconds.")
 
         # Create a dataframe from the json data
         df = pd.DataFrame(data)
@@ -31,24 +47,24 @@ class TreacheryToken(commands.Cog):
         end_date = df.index.max()
 
         # Define the start dates for weekly, monthly, 6 month, and yearly timeframes
-        start_date_weekly = end_date - timedelta(days=7) # CHANGE: subtract 7 days from end date
-        start_date_monthly = end_date - timedelta(days=30) # CHANGE: subtract 30 days from end date
-        start_date_6month = end_date - timedelta(days=182) # CHANGE: subtract 182 days from end date
-        start_date_yearly = end_date - timedelta(days=365) # CHANGE: subtract 365 days from end date
+        start_date_weekly = end_date - timedelta(days=7)
+        start_date_monthly = end_date - timedelta(days=30)
+        start_date_6month = end_date - timedelta(days=182)
+        start_date_yearly = end_date - timedelta(days=365)
 
         # Filter the dataframe for the defined timeframes
-        df_weekly = df.loc[start_date_weekly:end_date] # CHANGE: use start_date_weekly as lower bound
-        df_monthly = df.loc[start_date_monthly:end_date] # CHANGE: use start_date_monthly as lower bound
-        df_6month = df.loc[start_date_6month:end_date] # CHANGE: use start_date_6month as lower bound
-        df_yearly = df.loc[start_date_yearly:end_date] # CHANGE: use start_date_yearly as lower bound
+        df_weekly = df.loc[start_date_weekly:end_date]
+        df_monthly = df.loc[start_date_monthly:end_date]
+        df_6month = df.loc[start_date_6month:end_date]
+        df_yearly = df.loc[start_date_yearly:end_date]
 
         # Get the high and low prices for each timeframe
         high_w = df_weekly["value"].max()
         low_w = df_weekly["value"].min()
         high_m = df_monthly["value"].max()
         low_m = df_monthly["value"].min()
-        high_6m = df_6month["value"].max() # CHANGE: get the 6 month high price
-        low_6m = df_6month["value"].min() # CHANGE: get the 6 month low price
+        high_6m = df_6month["value"].max()
+        low_6m = df_6month["value"].min()
         high_y = df_yearly["value"].max()
         low_y = df_yearly["value"].min()
 
@@ -58,19 +74,19 @@ class TreacheryToken(commands.Cog):
         low_w = f"{low_w:,}"
         high_m = f"{high_m:,}"
         low_m = f"{low_m:,}"
-        high_6m = f"{high_6m:,}" # CHANGE: format the 6 month high price
-        low_6m = f"{low_6m:,}" # CHANGE: format the 6 month low price
+        high_6m = f"{high_6m:,}"
+        low_6m = f"{low_6m:,}"
         high_y = f"{high_y:,}"
         low_y = f"{low_y:,}"
 
         # Create a single embed object
         embed = discord.Embed(
-            color = discord.Color.blue(), # set the color of the embed
-            title = "WoW Token Price", # set the title of the embed
+            color = discord.Color.blue(),
+            title = "WoW Token Price",
         )
 
         # Set the timestamp of the embed with the timestamp attribute
-        embed.timestamp = datetime.now() # use the current time as the timestamp
+        embed.timestamp = datetime.now()
 
         # Add the current price as the first field of the embed, and set inline to False
         embed.add_field(name = "Current Price", value = f"```{current} gold```", inline = False)
@@ -79,8 +95,20 @@ class TreacheryToken(commands.Cog):
         embed.add_field(name = "Weekly Price", value = f"```High: {high_w} gold\nLow : {low_w} gold```", inline = True)
         embed.add_field(name = "Monthly Price", value = f"```High: {high_m} gold\nLow : {low_m} gold```", inline = True)
         embed.add_field(name = "\u200b", value = "\u200b", inline = False) # blank field
-        embed.add_field(name = "6 Month Price", value = f"```High: {high_6m} gold\nLow : {low_6m} gold```", inline = True) # CHANGE: add the 6 month price field
+        embed.add_field(name = "6 Month Price", value = f"```High: {high_6m} gold\nLow : {low_6m} gold```", inline = True)
         embed.add_field(name = "1 Year Price", value = f"```High: {high_y} gold\nLow : {low_y} gold```", inline = True)
 
+        # Get the start time of sending the message
+        start_time = time.time()
+
         # Send the embed message with the send method
-        await ctx.send(embed=embed) # send the embed as the message
+        await ctx.send(embed=embed)
+
+        # Get the end time of sending the message
+        end_time = time.time()
+
+        # Calculate the duration of sending the message
+        duration = end_time - start_time
+
+        # Print the duration of sending the message
+        print(f"Sending the message took {duration} seconds.")
