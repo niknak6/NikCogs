d93247281b33e61e845835712cfc2baa8657c1d2
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-16 22:09:44 -0500

    Update treacherytimers.py

--- treacherytimers/treacherytimers.py
+++ treacherytimers/treacherytimers.py
@@ -4,6 +4,8 @@ import requests
 from redbot.core import commands
 from datetime import datetime
 import pytz
+import re # Import the re module
+import json # Import the json module
 
 # Define the cog class
 class TreacheryTimers(commands.Cog):
@@ -32,17 +34,11 @@ class TreacheryTimers(commands.Cog):
 
             # Find all the sections with the raid reset timers
             import re
-            # Change this line
-            # sections = soup.find_all("section", id=re.compile("^US-group-raidresets"))
-            # To this line
-            sections = soup.find_all("section", id=re.compile("^.*-group-raidresets"))
+            sections = soup.find_all("section", id=re.compile("^US-group-raidresets"))
 
             # Check if the sections are not empty
             if sections:
                 # Initialize an empty list to store the timers and names
-                # Change this line
-                # mylist = [" ".join(section.stripped_strings) for section in sections]
-                # To this line
                 mylist = [" ".join(div.get_text() for div in section.find_all("div", class_="tiw-line")) for section in sections]
 
                 # Zip the list into pairs of name and timer
@@ -51,13 +47,30 @@ class TreacheryTimers(commands.Cog):
                 # Send the timers and names to the user
                 await ctx.send("Here are the raid reset timers for classic wow (Eastern Time):")
                 # Use a formatted string to display the pairs
-                # Change this line
-                # await ctx.send("\n".join(f"{name}: {timer}" for name, timer in pairs))
-                # To this line
                 await ctx.send("\n".join(f"**{name}**: {timer}" for name, timer in pairs))
             else:
                 # Handle the case when the sections are empty
-                await ctx.send("Sorry, I could not find any sections with the raid reset timers.")
+                # Define a variable that stores the url of the js file
+                js_url = "https://wow.zamimg.com/widgets/power.js"
+                # Make a request to the js url and get the response content
+                js_response = requests.get(js_url)
+                js_content = js_response.text
+                # Parse the js content and extract the array of objects that contains the raid reset timers
+                pattern = r"WH\.Wow\.TodayInWow\(\w+, (\[.*?\])\)"
+                match = re.search(pattern, js_content)
+                # Check if the match is not None
+                if match is None:
+                    # Handle the case when the js content does not have the raid reset timers
+                    await ctx.send("Sorry, I could not find the raid reset timers in the js file.")
+                else:
+                    # Get the array of objects from the match group and convert it to a Python list or dictionary
+                    js_data = json.loads(match.group(1))
+                    # Access the raid reset timers for the US region and group by indexing the list or dictionary
+                    us_timers = [group for group in js_data if group["regionId"] == "US"]
+                    # Send the raid reset timers to the user
+                    await ctx.send("Here are the raid reset timers for classic wow (Eastern Time):")
+                    # Use a formatted string to display the pairs
+                    await ctx.send("\n".join(f"**{line['name']}**: {line['endingShort']}" for group in us_timers for line in group["content"]["lines"].values()))
 
         else:
             # Send an error message if the response is not successful
