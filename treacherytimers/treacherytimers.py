dcffcdf63a93d28f92934ff380d9ef8298173e34
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-19 22:34:15 -0500

    Update treacherytimers.py

--- treacherytimers/treacherytimers.py
+++ treacherytimers/treacherytimers.py
@@ -1,61 +1,34 @@
+# Import the necessary libraries
+import asyncio
+import discord
 from redbot.core import commands
-from selenium import webdriver
-from bs4 import BeautifulSoup
+from pyppeteer import launch
 
-class TreacheryTimers(commands.Cog):
-    """A cog that shows raid reset timers from https://classicraidreset.com/US/SoD"""
+# Define a class for the cog
+class RaidTimerCog(commands.Cog):
+    """A cog that displays raid timers and dates from https://classicraidreset.com/US/SoD"""
 
     def __init__(self, bot):
         self.bot = bot
 
+    # Define a command to get the raid timers and dates
     @commands.command()
-    async def timers(self, ctx):
-        """This shows the raid reset timers from the website."""
+    async def raidtimer(self, ctx):
+        """Get the raid timers and dates from https://classicraidreset.com/US/SoD"""
 
-        # Start a new Chrome session
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")  # Run Chrome in headless mode (without opening GUI)
-        driver = webdriver.Chrome(options=options)
+        # Launch a browser and a page
+        browser = await launch()
+        page = await browser.newPage()
 
-        # Define the URL
-        URL = "https://classicraidreset.com/US/SoD"
+        # Go to the website and wait for the calendar to load
+        await page.goto("https://classicraidreset.com/US/SoD")
+        await page.waitForSelector("#calendar-element")
 
-        try:
-            # Open the URL
-            driver.get(URL)
+        # Get the HTML content of the calendar
+        calendar = await page.querySelectorEval("#calendar-element", "element => element.innerHTML")
 
-            # Wait for the page to load (you might need to adjust this delay)
-            driver.implicitly_wait(10)
+        # Close the browser
+        await browser.close()
 
-            # Get the page source
-            page_source = driver.page_source
-
-            # Use BeautifulSoup to parse the page source
-            soup = BeautifulSoup(page_source, "html.parser")
-
-            # Find the div element that contains the calendar data
-            calendar = soup.find("div", id="calendar-element")
-
-            # Find the script element inside the calendar div that has a wire:snapshot attribute
-            script = calendar.find("script", attrs={"wire:snapshot": True})
-
-            # Get the value of the wire:snapshot attribute as a string
-            snapshot = script["wire:snapshot"]
-
-            # Process the snapshot as needed (remaining code similar to your original approach)
-            data = json.loads(snapshot)
-            events = data["data"]["events"]
-            event_list = []
-
-            for event in events:
-                event_list.append(format_event(event))
-
-            event_str = "\n".join(event_list)
-            await ctx.send(event_str)
-
-        except Exception as e:
-            await ctx.send(f"An error occurred: {str(e)}")
-
-        finally:
-            # Close the browser session after scraping
-            driver.quit()
+        # Send the calendar as a message
+        await ctx.send(f"Here is the raid timer and date calendar from https://classicraidreset.com/US/SoD:\n```html\n{calendar}\n```")
