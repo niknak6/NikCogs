67834684e689dcf8b22c2d009d0da802694c5af1
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-24 17:08:07 -0500

    Update treacherytimers.py

--- treacherytimers/treacherytimers.py
+++ treacherytimers/treacherytimers.py
@@ -1,44 +1,22 @@
-# Import lxml.html and json libraries
-import lxml.html
-import json
-
-# Import requests library
 import requests
+from discord.ext import commands
 
-# Import commands module from redbot.core
-from redbot.core import commands
-
-# Define a class named TreacheryTimers that inherits from commands.Cog
 class TreacheryTimers(commands.Cog):
-    """A cog that shows the raid reset days for WoW Season of Discovery"""
-
-    # Define an __init__ method that takes a bot parameter and assigns it to self.bot
     def __init__(self, bot):
         self.bot = bot
 
-    # Define a commands.command decorator that defines a command named timers
     @commands.command()
     async def timers(self, ctx):
-        """Shows the raid reset days for WoW Season of Discovery"""
-        # Get the html content of the website and assign it to html
-        # Change the URL to the one you specified
-        html = requests.get('https://classicraidreset.com/US/SoD').content
+        url = "https://www.wowhead.com/classic"
+        response = requests.get(url)
+        html_source = response.text
 
-        # Parse the html source code of the website using lxml.html
-        tree = lxml.html.fromstring(html)
+        # Split the source into 2000 character chunks
+        chunks = [html_source[i:i+2000] for i in range(0, len(html_source), 2000)]
 
-        # Find the wire:snapshot element and get the events attribute as a JSON string
-        snapshot = tree.xpath('//div[@wire:snapshot]/@wire:snapshot')[0]
+        # Send each chunk to the channel
+        for chunk in chunks:
+            await ctx.send(f'```{chunk}```')
 
-        # Load the JSON string as a Python dictionary and get the data property, which contains the events array
-        events = json.loads(snapshot)
-        events = events['data']['s'] # Access the events array by the 's' key
-
-        # Filter the events by the type property, which should be instance
-        events = [event for event in events if event['type'] == 'instance']
-
-        # Format the start and title properties of each event as a string, separated by a space, and join them with newlines
-        output = '\n'.join([f"{event['start']} {event['title']}" for event in events])
-
-        # Send the formatted string to the channel where the command was used, using ctx.send
-        await ctx.send(output)
+def setup(bot):
+    bot.add_cog(TreacheryTimers(bot))
