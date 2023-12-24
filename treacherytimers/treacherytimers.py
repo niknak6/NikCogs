d788a1756c29af5a026dc498e2ffc1c273384109
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-24 18:08:20 -0500

    Update treacherytimers.py

--- treacherytimers/treacherytimers.py
+++ treacherytimers/treacherytimers.py
@@ -27,7 +27,8 @@ class TreacheryTimers(commands.Cog):
             web_page_source = response.text
 
             # Define a regex pattern to match the JSON data for the raid reset timers
-            pattern = r"\[{\\"id\\":\\"dungeons-and-raids\\",.+?}(?=])\];"
+            # Use a normal string literal and escape both the backslashes and the double quotes
+            pattern = "[{\\\"id\\\":\\\"dungeons-and-raids\\\",.+?}(?=])];"
 
             # Search for the pattern in the web page source
             match = re.search(pattern, web_page_source)
