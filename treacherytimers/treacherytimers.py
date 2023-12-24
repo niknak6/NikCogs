ad5fe39465340c96d79437957008439dcd987877
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-24 17:53:24 -0500

    Update treacherytimers.py

--- treacherytimers/treacherytimers.py
+++ treacherytimers/treacherytimers.py
@@ -23,64 +23,64 @@ class TreacheryTimers(commands.Cog):
 
         # Check if the response status code is 200 (OK)
         if response.status_code == 200:
-            # Parse the response content using BeautifulSoup
-            soup = BeautifulSoup(response.content, "html.parser")
+            # Get the response content as a string
+            web_page_source = response.text
 
-            # Find the element that has the id 'today-in-wow'
-            today_in_wow_element = soup.find(id="today-in-wow")
+            # Define a regex pattern to match the JSON data for the raid reset timers
+            pattern = r"\[{\\"id\\":\\"dungeons-and-raids\\",.+?}]);"
 
-            # Find the script element inside the today-in-wow element
-            script_element = today_in_wow_element.script
+            # Search for the pattern in the web page source
+            match = re.search(pattern, web_page_source)
 
-            # Get the text content of the script element
-            script_text = script_element.text
+            # Check if the pattern was found
+            if match:
+                # Get the matched substring
+                json_data = match.group()
 
-            # Find the substring that contains the JSON data for the raid reset timers
-            start_index = script_text.find("[{")
-            end_index = script_text.find("}]);")
-            json_data = script_text[start_index:end_index+2]
+                # Convert the JSON data to a Python object
+                data = json.loads(json_data)
 
-            # Convert the JSON data to a Python object
-            data = json.loads(json_data)
+                # Loop through the data
+                for item in data:
+                    # Get the name and region of the item
+                    name = item["name"]
+                    region = item["regionId"]
 
-            # Loop through the data
-            for item in data:
-                # Get the name and region of the item
-                name = item["name"]
-                region = item["regionId"]
+                    # Print the name and region
+                    print(name, region)
 
-                # Print the name and region
-                print(name, region)
+                    # Get the groups of the item
+                    groups = item["groups"]
 
-                # Get the groups of the item
-                groups = item["groups"]
+                    # Loop through the groups
+                    for group in groups:
+                        # Get the content of the group
+                        content = group["content"]
 
-                # Loop through the groups
-                for group in groups:
-                    # Get the content of the group
-                    content = group["content"]
+                        # Get the lines of the content
+                        lines = content["lines"]
 
-                    # Get the lines of the content
-                    lines = content["lines"]
+                        # Loop through the lines
+                        for line in lines.values():
+                            # Get the raid name, ending, and url of the line
+                            raid_name = line["name"]
+                            raid_ending = line["ending"]
+                            raid_url = line["url"]
 
-                    # Loop through the lines
-                    for line in lines.values():
-                        # Get the raid name, ending, and url of the line
-                        raid_name = line["name"]
-                        raid_ending = line["ending"]
-                        raid_url = line["url"]
+                            # Print the raid name, ending, and url
+                            print(raid_name, raid_ending, raid_url)
 
-                        # Print the raid name, ending, and url
-                        print(raid_name, raid_ending, raid_url)
+                # Create an embed object
+                embed = discord.Embed(title="Classic Raid Reset Timers", description="The raid reset timers for the classic season of discovery")
 
-            # Create an embed object
-            embed = discord.Embed(title="Classic Raid Reset Timers", description="The raid reset timers for the classic season of discovery")
+                # Add the name, raid name, ending, and url as fields
+                embed.add_field(name=name, value=f"{raid_name}: {raid_ending} [More info]")
 
-            # Add the name, raid name, ending, and url as fields
-            embed.add_field(name=name, value=f"{raid_name}: {raid_ending} [More info]")
-
-            # Send the embed to the channel
-            await ctx.send(embed=embed)
+                # Send the embed to the channel
+                await ctx.send(embed=embed)
+            else:
+                # Handle the error if the pattern was not found
+                print("Error: No JSON data found")
         else:
             # Handle the error if the response status code is not 200
             print(f"Error: {response.status_code}")
