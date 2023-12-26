5a86140fc710a84e52f734983fb3763768afedc9
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-26 01:50:49 -0500

    Update treacherynews.py

--- treacherynews/treacherynews.py
+++ treacherynews/treacherynews.py
@@ -1,8 +1,8 @@
 # Import the required modules
 from redbot.core import commands
 from PIL import Image, ImageDraw, ImageFont
-import discord # Import the discord module
-import textwrap # Import the textwrap module
+import discord
+import textwrap
 
 # Define the cog class
 class TreacheryNews(commands.Cog):
@@ -28,55 +28,34 @@ class TreacheryNews(commands.Cog):
         # Draw a horizontal line below the headline
         draw.line((0, 100, 800, 100), fill=(0, 0, 0))
 
-        # Draw a vertical line between the articles
-        draw.line((400, 150, 400, 600), fill=(0, 0, 0))
-
         # Draw the headline with a larger font and centered alignment
-        draw.text((400, 50), "Treachery News", fill=(0, 0, 0), font=font, anchor="mm") # Use the anchor argument to center the text
-
-        # Draw a box for the articles on the left side of the image
-        draw.rectangle((50, 150, 350, 450), fill=(255, 255, 255), outline=(0, 0, 0))
-
-        # Define the text for the left box
-        text = "Article 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Article 2: Sed quis nisi quis augue gravida fermentum. Article 3: Quisque euismod leo at nisl ullamcorper, ac aliquet erat lacinia."
-
-        # Wrap the text into lines that fit within the box
-        lines = textwrap.wrap(text, width=40)
+        draw.text((400, 50), "Treachery News", fill=(0, 0, 0), font=font, anchor="mm")
 
         # Define the margins and the spacing
         margin = 10
         spacing = 10
 
-        # Draw the lines with a smaller font and center alignment
-        x = 200 # Use the center of the box as the x coordinate
-        y = 150 + margin
-        for line in lines:
-            draw.text((x, y), line, fill=(0, 0, 0), font=font, align="center", anchor="ma") # Use the align argument to center the text horizontally and the anchor argument to center the text vertically
-            y += font.getbbox(line)[3] + spacing # Increase the y coordinate by the bottom coordinate of the bounding box and the spacing
+        # Draw a 2x2 grid of boxes for the articles
+        for i in range(2):
+            for j in range(2):
+                draw.rectangle((50 + 400 * i, 150 + 250 * j, 350 + 400 * i, 400 + 250 * j), fill=(255, 255, 255), outline=(0, 0, 0))
 
-        # Draw a box for the articles on the right side of the image
-        draw.rectangle((450, 150, 750, 450), fill=(255, 255, 255), outline=(0, 0, 0))
+        # Define the text for each box
+        texts = ["Article 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
+                 "Article 2: Sed quis nisi quis augue gravida fermentum.", 
+                 "Article 3: Quisque euismod leo at nisl ullamcorper, ac aliquet erat lacinia.",
+                 "Article 4: Fusce vitae nisi quis eros tincidunt consequat."]
 
-        # Define the text for the right box
-        text = "Article 4: Fusce vitae nisi quis eros tincidunt consequat. Article 5: Morbi id magna vitae nunc sagittis tristique. Article 6: Praesent vel neque quis elit faucibus blandit."
-
-        # Wrap the text into lines that fit within the box
-        lines = textwrap.wrap(text, width=40)
-
-        # Define the margins and the spacing
-        margin = 10
-        spacing = 10
-
-        # Draw the lines with a smaller font and center alignment
-        x = 600 # Use the center of the box as the x coordinate
-        y = 150 + margin
-        for line in lines:
-            draw.text((x, y), line, fill=(0, 0, 0), font=font, align="center", anchor="ma") # Use the align argument to center the text horizontally and the anchor argument to center the text vertically
-            y += font.getbbox(line)[3] + spacing # Increase the y coordinate by the bottom coordinate of the bounding box and the spacing
-
-        # Draw a box for the New York Times logo on the top left corner of the image
-        draw.rectangle((50, 10, 150, 90), fill=(255, 255, 255), outline=(0, 0, 0))
-
+        # Draw the text in each box
+        for i in range(2):
+            for j in range(2):
+                text = texts[i * 2 + j]
+                lines = textwrap.wrap(text, width=40)
+                x = 200 + 400 * i
+                y = 150 + 250 * j + margin
+                for line in lines:
+                    draw.text((x, y), line, fill=(0, 0, 0), font=font, align="center", anchor="ma")
+                    y += font.getbbox(line)[3] + spacing
 
         # Save the image
         image.save("news.png")
