e16df0445329f70d22c55b7267fc7b98cfcaec33
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-26 02:13:28 -0500

    Update treacherynews.py

--- treacherynews/treacherynews.py
+++ treacherynews/treacherynews.py
@@ -21,20 +21,20 @@ class TreacheryNews(commands.Cog):
         box_width, box_height = (image_width - 3 * 20) // 2, (image_height - 100 - 3 * 20) // 2
         margin = 20
 
-        # Create a blank image with a light gray background
-        image = Image.new("RGB", (image_width, image_height), (240, 240, 240))
+        # Create a blank image with a transparent background
+        image = Image.new("RGBA", (image_width, image_height))
 
         # Create a draw object
         draw = ImageDraw.Draw(image)
 
         # Load the default font
-        font = ImageFont.load_default()
+        font = ImageFont.truetype("arialbd.ttf", 15)  # Use a bold font
 
         # Draw the headline with a larger font and centered alignment
-        draw.text((image_width // 2, 30), "Treachery News", fill=(0, 0, 0), font=font, anchor="mm")
+        draw.text((image_width // 2, 30), "Treachery News", fill="hotpink", font=font, anchor="mm")
 
         # Draw a horizontal line below the headline
-        draw.line((0, 60, image_width, 60), fill=(0, 0, 0))
+        draw.line((0, 60, image_width, 60), fill="hotpink", width=2)  # Increased the line width
 
         # Define the text for each box
         texts = ["Article 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
@@ -42,26 +42,17 @@ class TreacheryNews(commands.Cog):
                  "Article 3: Quisque euismod leo at nisl ullamcorper, ac aliquet erat lacinia.",
                  "Article 4: Fusce vitae nisi quis eros tincidunt consequat."]
 
-        # Calculate the vertical space available for the boxes
-        available_space = image_height - 80  # 80 is the space taken by the headline and the line
-
-        # Calculate the total height of the boxes and the margins
-        total_height = 2 * box_height + 3 * margin
-
-        # Calculate the extra space that should be added to the top and bottom margins
-        extra_space = (available_space - total_height) // 2
-
         # Draw a 2x2 grid of boxes for the articles with margins and draw the text in each box
         for i in range(2):
             for j in range(2):
                 # Calculate the position of the box
                 left = margin + (box_width + margin) * i
-                top = 80 + extra_space + (box_height + margin) * j  # Added the extra space to the top margin
+                top = 80 + (box_height + margin) * j  # Added the extra space to the top margin
                 right = left + box_width
                 bottom = top + box_height
 
                 # Draw the box
-                draw.rectangle((left, top, right, bottom), fill=(255, 255, 255), outline=(0, 0, 0))
+                draw.rectangle((left, top, right, bottom), outline="hotpink", width=2)  # Increased the line width
 
                 # Get the text for the box
                 text = texts[i * 2 + j]
@@ -73,7 +64,7 @@ class TreacheryNews(commands.Cog):
                 x = left + box_width // 2
                 y = top + margin
                 for line in lines:
-                    draw.text((x, y), line, fill=(0, 0, 0), font=font, align="center", anchor="ma")
+                    draw.text((x, y), line, fill="hotpink", font=font, align="center", anchor="ma")  # Changed the text color to hot pink
                     y += font.getbbox(line)[3] + 10  # Increase the y coordinate by the height of the line and some spacing
 
         # Save the image
