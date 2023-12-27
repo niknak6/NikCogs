241039ab5b1b6c12be2783e49df31616ca969845
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-27 03:10:12 -0500

    Update treacherytimers.py

--- treacherytimers/treacherytimers.py
+++ treacherytimers/treacherytimers.py
@@ -54,17 +54,19 @@ class TreacheryTimers(commands.Cog):
         [raid_reset_embed.add_field(name=raid_name, value=f"{raid_ending_time} (Resets at {formatted_reset_time})") for raid_name, (raid_ending_time, formatted_reset_time) in earliest_resets.items()]
         await ctx.send(embed=raid_reset_embed)
 
-    @commands.command()
-    async def mplus(self, ctx):
-        """Fetches and parses the web page https://keystone.guru/affixes and displays the WoW Mythic+ affixes by week"""
-        affix_matches = self.fetch_and_parse("https://keystone.guru/affixes", r"(\d{4}/\w{3}/\d{2}).+?data-original-title=\"(.+?)\".+?data-original-title=\"(.+?)\".+?data-original-title=\"(.+?)\"")
-        if not affix_matches:
-            await ctx.send("No data can be found. Please check keystone.guru/affixes to ensure times are visible. If the data is there, send a message to Nik.")
-            return
+@commands.command()
+async def mplus(self, ctx):
+    """Fetches and parses the web page https://keystone.guru/affixes and displays the WoW Mythic+ affixes by week"""
+    # Use a more specific pattern to match the date and the affixes
+    affix_matches = self.fetch_and_parse("https://keystone.guru/affixes", r"(\d{4}/\w{3}/\d{2}).+?affix_icon_(.+?)\".+?affix_icon_(.+?)\".+?affix_icon_(.+?)\"")
+    if not affix_matches:
+        await ctx.send("No data can be found. Please check keystone.guru/affixes to ensure times are visible. If the data is there, send a message to Nik.")
+        return
 
-        affix_embed = discord.Embed(title="WoW Mythic+ Affixes by Week", description="")
-        for match in affix_matches:
-            date, affix1, affix2, affix3 = match.split()
-            affix_embed.add_field(name=date, value=f"+2: {affix1}, +7: {affix2}, +14: {affix3}", inline=False)
+    affix_embed = discord.Embed(title="WoW Mythic+ Affixes by Week", description="")
+    for match in affix_matches:
+        # Split the match and use the groups to create the embed fields
+        date, affix1, affix2, affix3 = match.split()
+        affix_embed.add_field(name=date, value=f"+2: {affix1}, +7: {affix2}, +14: {affix3}", inline=False)
 
-        await ctx.send(embed=affix_embed)
+    await ctx.send(embed=affix_embed)
