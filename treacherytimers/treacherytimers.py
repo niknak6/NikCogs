a558074fa5f6ebfcdf4fa4774a3df39e53f41ed9
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-29 02:30:35 -0500

    Update treacherytimers.py

--- treacherytimers/treacherytimers.py
+++ treacherytimers/treacherytimers.py
@@ -34,9 +34,9 @@ class TreacheryTimers(commands.Cog):
     @commands.command()
     async def timers(self, ctx):
         """Fetches and parses the web page https://www.wowhead.com/classic and displays the classic raid reset timers"""
-        # Use a more specific regular expression to extract the JSON data from the string
-        # Remove the extra backslash before the closing parenthesis at position 57
-        raid_reset_data = self.fetch_and_parse("https://www.wowhead.com/classic", r'new WH\.Wow\.TodayInWow\(WH\.ge\(\\\'today-in-wow\\\'\), (\[{"id":"dungeons-and-raids".*?"regionId":"\w{2}".*?"groups":\[{"id":"raidresets".*?"content":\{"lines":\{.*?\}\}\}\]\}\]\)+\);')
+        # Use the re.escape function to automatically escape any special characters in the pattern
+        pattern = re.escape("new WH.Wow.TodayInWow(WH.ge(\'today-in-wow\'), [{\"id\":\"dungeons-and-raids\".*?\"regionId\":\"\\w{2}\".*?\"groups\":[{\"id\":\"raidresets\".*?\"content\":{\"lines\":{.*?}}}]})+);")
+        raid_reset_data = self.fetch_and_parse("https://www.wowhead.com/classic", pattern)
         if not raid_reset_data:
             await ctx.send("No data can be found. Please check wowhead.com/classic to ensure times are visible. If the data is there, send a message to Nik.")
             return
