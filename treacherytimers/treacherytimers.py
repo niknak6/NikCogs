ca76bca16db6229a8bacbda3aa70d9a0a0d43d9c
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-12-29 01:20:39 -0500

    Update treacherytimers.py

--- treacherytimers/treacherytimers.py
+++ treacherytimers/treacherytimers.py
@@ -34,7 +34,7 @@ class TreacheryTimers(commands.Cog):
     async def timers(self, ctx):
         """Fetches and parses the web page https://www.wowhead.com/classic and displays the classic raid reset timers"""
         # Use a regular expression to extract the JSON data from the string
-        raid_reset_data = self.fetch_and_parse("https://www.wowhead.com/classic", r'new WH\.Wow\.TodayInWow\(WH\.ge\(\'today-in-wow\'\), (.*?)}\]\);')
+        raid_reset_data = self.fetch_and_parse("https://www.wowhead.com/classic", r'new WH\.Wow\.TodayInWow\(WH\.ge\(\'today-in-wow\'\), (.*?}\]\)\);)')
         if not raid_reset_data:
             await ctx.send("No data can be found. Please check wowhead.com/classic to ensure times are visible. If the data is there, send a message to Nik.")
             return
