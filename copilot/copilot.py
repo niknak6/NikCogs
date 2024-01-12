d2e632da3f79e704edaa559dc378b82e6ddef057
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-01-11 23:34:44 -0500

    Update copilot.py

--- copilot/copilot.py
+++ copilot/copilot.py
@@ -13,7 +13,7 @@ class Copilot(commands.Cog):
 
     async def create_chatbot(self):
         """Creates a chatbot instance using the cookies file"""
-        cookies = json.loads(open("bing_cookies.json", encoding="utf-8").read())
+        cookies = json.loads(open("/bing_cookies.json", encoding="utf-8").read())
         self.chatbot = await Chatbot.create(cookies=cookies)
 
     async def create_imagegen(self):
