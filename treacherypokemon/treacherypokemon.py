6d336ce57e955ba178455911a757b676db05d93d
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-01-17 19:16:35 -0500

    Update treacherypokemon.py

--- treacherypokemon/treacherypokemon.py
+++ treacherypokemon/treacherypokemon.py
@@ -1,4 +1,4 @@
-from redbot.core import commands
+from redbot.core import commands, config
 import requests
 import random
 import discord
@@ -10,6 +10,10 @@ class TreacheryPokemon(commands.Cog):
         self.bot = bot
         self.current_pokemon = None
         self.current_sprite = None
+        # Create a Config object for this cog
+        self.config = config.Config.get_conf(self, identifier=1234567890)
+        # Register a user setting for storing the starter Pokémon
+        self.config.register_user(starter=None)
 
     @commands.command()
     async def spawn(self, ctx):
@@ -35,6 +39,54 @@ class TreacheryPokemon(commands.Cog):
             self.current_sprite = None
         else:
             await ctx.send("There's no Pokémon to catch. Use the `spawn` command to spawn one.")
+
+    @commands.command(name="pickstarter")
+    async def pickstarter(self, ctx):
+        """Picks a starter Pokémon from a list of options."""
+        # Get the user ID of the command author
+        user_id = ctx.author.id
+        # Get the starter Pokémon stored for this user, if any
+        starter = await self.config.user(ctx.author).starter()
+        # If the user already has a starter, inform them and return
+        if starter is not None:
+            await ctx.send(f"You already have a starter Pokémon: {starter.capitalize()}.")
+            return
+        # Otherwise, create a list of starter Pokémon options
+        starters = [
+            "Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Eevee",
+            "Chikorita", "Cyndaquil", "Totodile",
+            "Treecko", "Torchic", "Mudkip",
+            "Turtwig", "Chimchar", "Piplup",
+            "Snivy", "Tepig", "Oshawott",
+            "Chespin", "Fennekin", "Froakie",
+            "Rowlet", "Litten", "Popplio",
+            "Grookey", "Scorbunny", "Sobble"
+        ]
+        # Create a message with the list of options and a reaction menu
+        message = "Please pick a starter Pokémon from the following options:\n"
+        for i, starter in enumerate(starters):
+            message += f"{i+1}. {starter}\n"
+        message += "React with the corresponding number to make your choice."
+        # Send the message and add the number reactions
+        msg = await ctx.send(message)
+        for i in range(len(starters)):
+            await msg.add_reaction(str(i+1) + "\N{combining enclosing keycap}")
+        # Wait for the user to react with a valid choice
+        def check(reaction, user):
+            return user == ctx.author and reaction.message.id == msg.id and reaction.emoji in [str(i+1) + "\N{combining enclosing keycap}" for i in range(len(starters))]
+        try:
+            reaction, user = await self.bot.wait_for("reaction_add", timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await ctx.send("You took too long to pick a starter. Please try again.")
+        else:
+            # Get the index of the chosen starter from the reaction emoji
+            index = int(reaction.emoji[0]) - 1
+            # Get the name of the chosen starter from the list
+            starter = starters[index]
+            # Store the starter Pokémon for this user in the config
+            await self.config.user(ctx.author).starter.set(starter)
+            # Inform the user of their choice
+            await ctx.send(f"You picked {starter.capitalize()} as your starter Pokémon!")
 
 def setup(bot):
     bot.add_cog(TreacheryPokemon(bot))
