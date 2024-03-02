1e2936519b066360ddcab6fa71f0abd28dd8cffd
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-03-02 14:31:37 -0500

    Update tiktokcog.py

--- tiktokcog/tiktokcog.py
+++ tiktokcog/tiktokcog.py
@@ -43,8 +43,8 @@ class TikTokCog(commands.Cog):
     def construct_new_url(self, url_match, platform):
         # Add a condition to check if the platform is instagram and the URL contains reel
         if platform == 'instagram' and 'reel' in url_match.group(5): # NEW
-            # Replace the original domain with 'ddinstagram.com/reel'
-            return url_match.group(0).replace(url_match.group(4), 'ddinstagram.com/reel') # UPDATED
+            # Replace the original domain with 'ddinstagram.com'
+            return url_match.group(0).replace(url_match.group(4), 'ddinstagram.com') # UPDATED
         else:
             # Use the original logic
             return (url_match.group(1) or 'https://') + (url_match.group(2) or '') + self.new_domains[platform] + url_match.group(5)
