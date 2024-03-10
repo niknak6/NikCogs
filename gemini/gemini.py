6506051413939a025dc7019679e21174fa839412
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-03-10 14:36:13 -0400

    Update gemini.py

--- gemini/gemini.py
+++ gemini/gemini.py
@@ -22,8 +22,8 @@ class Gemini(commands.Cog):
             # Settings for the text model
             text_temperature=1.0,
             text_top_p=1,
-            text_top_k=1,
-            text_max_output_tokens=2048,
+            text_top_k=40,
+            text_max_output_tokens=8192,
             text_safety_settings=[{"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"}, {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"}, {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"}, {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}],
             # Settings for the image model
             image_temperature=0.4,
