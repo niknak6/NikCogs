d3627420ca41c9e39536198b574f6b318c3737a0
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-03-09 23:17:23 -0500

    Update gemini.py

--- gemini/gemini.py
+++ gemini/gemini.py
@@ -27,8 +27,8 @@ class Gemini(commands.Cog):
             text_safety_settings=[{"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"}, {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"}, {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"}, {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}],
             # Settings for the image model
             image_temperature=0.4,
-            image_top_p=0.3,
-            image_top_k=16,
+            image_top_p=0,
+            image_top_k=1,
             image_max_output_tokens=2048,
             image_safety_settings=[{"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"}, {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"}, {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"}, {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}]
         )
