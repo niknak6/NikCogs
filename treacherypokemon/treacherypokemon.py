26bc55f52430dfd44304311fb9cd58c908dc94d8
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-03-22 23:10:09 -0400

    Update treacherypokemon.py

--- treacherypokemon/treacherypokemon.py
+++ treacherypokemon/treacherypokemon.py
@@ -435,12 +435,13 @@ class TreacheryPokemon(commands.Cog):
                         # Update the embed with the new sprite
                         battle_embed.set_image(url="attachment://combined_sprite.png")
                         await battle_message.edit(embed=battle_embed, attachments=[combined_image_file])
-                        await asyncio.sleep(2)  # Add a cooldown
+                        await asyncio.sleep(3)  # Add a 3-second cooldown
                         battle_embed.set_field_at(hp_field_index, name=f"{player_display}'s {new_pokemon} HP", value=f"{new_pokemon_hp}", inline=True)
                     else:
                         winner = opponent.display_name if player_display == ctx.author.display_name else ctx.author.display_name
                         battle_embed.clear_fields()
                         battle_embed.description += f"\n**{winner} wins the battle!**"
+                        battle_embed.set_image(url=None)  # Remove the image from the embed
                         await battle_message.edit(embed=battle_embed)
                         del self.battles[ctx.author.id], self.battles[opponent.id]
                         return
