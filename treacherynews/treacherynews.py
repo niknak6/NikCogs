47228ff170f971cfe21d65b61a4a946a3a458488
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-03-25 22:27:40 -0400

    Update treacherynews.py

--- treacherynews/treacherynews.py
+++ treacherynews/treacherynews.py
@@ -2,7 +2,7 @@ from redbot.core import commands, Config
 import tiktokpy
 from tiktokpy.utils.client import Client
 
-class TikTokDownloader(commands.Cog):
+class TreacheryNews(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890, force_registration=True)
