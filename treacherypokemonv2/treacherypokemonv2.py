4957c2fe1f6585779016bd2f5b1eeb8436399041
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-03-27 19:35:09 -0400

    Update treacherypokemonv2.py

--- treacherypokemonv2/treacherypokemonv2.py
+++ treacherypokemonv2/treacherypokemonv2.py
@@ -8,17 +8,22 @@ class TreacheryPokemonV2(commands.Cog):
         self.bot = bot
         self.client = pokepy.V2Client()
 
-    @commands.command()
-    async def v2spawn(self, ctx):
-        # Generate a random pokemon number between 1 and 1025
-        pokemon_number = random.randint(1, 1025)
-        # Fetch the pokemon
-        pokemon = await self.client.get_pokemon(pokemon_number)
-        # Create the embed
-        embed = discord.Embed(title="A wild Pokémon has appeared!", color=discord.Color.green())
-        embed.set_thumbnail(url=pokemon.sprites.front_default)
-        # Send the embed
-        await ctx.send(embed=embed)
+        @commands.command()
+        async def v2spawn(self, ctx):
+            # Generate a random pokemon number between 1 and 1025
+            pokemon_number = random.randint(1, 1025)
+            # Fetch the pokemon
+            pokemon_list = self.client.get_pokemon(pokemon_number)
+            # Assuming the first pokemon in the list is the one you want
+            pokemon = pokemon_list[0] if pokemon_list else None
+            if pokemon:
+                # Create the embed
+                embed = discord.Embed(title="A wild Pokémon has appeared!", color=discord.Color.green())
+                embed.set_thumbnail(url=pokemon.sprites.front_default)
+                # Send the embed
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send("No Pokémon found.")
 
 def setup(bot):
     bot.add_cog(PokemonSpawn(bot))
