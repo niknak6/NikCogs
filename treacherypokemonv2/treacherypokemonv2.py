53ab21886b39978f594e0f1933afea0e3487e21e
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-03-27 20:13:08 -0400

    Update treacherypokemonv2.py

--- treacherypokemonv2/treacherypokemonv2.py
+++ treacherypokemonv2/treacherypokemonv2.py
@@ -6,10 +6,9 @@ class TreacheryPokemonV2(commands.Cog):
     @commands.command()
     async def v2spawn(self, ctx):
         try:
-            pokemon_number = random.randint(1, 1050)
-            artwork_url = pb.SpriteResource('pokemon', pokemon_number, other=True, official_artwork=True).url
-            embed = discord.Embed(title="A wild Pokémon appeared!").set_image(url=artwork_url)
-            await ctx.send(embed=embed)
+            num = random.randint(1, 1050)
+            url = pb.SpriteResource('pokemon', num, other=True, official_artwork=True).url
+            await ctx.send(embed=discord.Embed(title="A wild Pokémon appeared!").set_image(url=url))
         except pb.HTTPError as e:
             await ctx.send("Sorry, no artwork found for this Pokémon." if e.response.status_code == 404 else "An error occurred while fetching the Pokémon artwork.")
 
