28d9522d03a44656a11818bacd859be54d075bf0
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-04-03 17:47:21 -0400

    Update pinfill.py

--- pinfill/pinfill.py
+++ pinfill/pinfill.py
@@ -18,9 +18,10 @@ class PinFill(commands.Cog):
                 if response.status == 200:
                     html = await response.text()
                     soup = BeautifulSoup(html, 'html.parser')
-                    # Find the section containing Elemental Storms information
-                    storms_section = soup.find('section', {'data-tiw-section': 'group-elemental-storms'})
+                    # Find the section by ID
+                    storms_section = soup.find(id='US-group-elemental-storms')
                     if storms_section:
+                        # Find all upcoming storms
                         upcoming_storms = storms_section.find_all('section', class_='tiw-line-name elemental-storm tiw-upcoming')
                         upcoming_times = storms_section.find_all('section', class_='tiw-line-ending elemental-storm tiw-upcoming tiw-active')
                         if upcoming_storms and upcoming_times:
