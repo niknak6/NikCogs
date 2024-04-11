b55a189582e3e6a252d474053eb7787328b51778
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-04-11 16:19:16 -0400

    betaalpha

--- betaalpha/__init__.py
+++ betaalpha/__init__.py
@@ -1,7 +1,7 @@
 # Import the cog class
-from .pinfill import PinFill
+from .betaalpha import BetaAlpha
 
 # Define the setup function
 async def setup(bot):
     # Add the cog to the bot
-    await bot.add_cog(PinFill(bot))
+    await bot.add_cog(BetaAlpha(bot))
