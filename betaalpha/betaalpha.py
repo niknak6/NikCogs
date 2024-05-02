74ecfcebade38333cbe408d623e9763a33b3ede6
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-05-01 20:26:01 -0400

    Update betaalpha.py

--- betaalpha/betaalpha.py
+++ betaalpha/betaalpha.py
@@ -9,7 +9,7 @@ class BetaAlpha(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.is_conversation = False
+        self.is_conversation = True
         self.history = []
 
     @commands.command()
