ed549fdb256ead65c9e8e585cc06eba21d1242b1
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-05-26 11:06:26 -0400

    Delete treacherypokemonv2 directory

--- treacherypokemonv2/__init__.py
+++
@@ -1,7 +0,0 @@
-# Import the cog class
-from .treacherypokemonv2 import TreacheryPokemonV2
-
-# Define the setup function
-async def setup(bot):
-    # Add the cog to the bot
-    await bot.add_cog(TreacheryPokemonV2(bot))
