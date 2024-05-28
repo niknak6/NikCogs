9fd66148149b88fe6f98a8e29bc666af7f98570d
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-05-28 13:48:45 -0400

    Update tiktokcog.py

--- tiktokcog/tiktokcog.py
+++ tiktokcog/tiktokcog.py
@@ -17,7 +17,7 @@ class TikTokCog(commands.Cog):
             'instagram': re.compile(r"(https?://)?((\w+\.)?(\w+)\.)?instagram.com/reel/(\S*)") # UPDATED
         }
         self.new_domains = {
-            'tiktok': 'vxtiktok.com/',
+            'tiktok': 'tnktok.com/',
             'twitter': 'vxtwitter.com/',
             'x': 'fixvx.com/',
             'instagram': 'ddinstagram.com/reel/'
