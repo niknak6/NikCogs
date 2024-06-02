d8c8e399b8f57b4de466a0491950afe13206d6d6
niknak6 <113207452+niknak6@users.noreply.github.com>
2024-06-02 15:38:10 -0400

    Delete treacheryaffixes directory

--- treacheryaffixes/__init__.py
+++
@@ -1,7 +0,0 @@
-# Import the cog class
-from .treacheryaffixes import TreacheryAffixes
-
-# Define the setup function
-async def setup(bot):
-    # Add the cog to the bot
-    await bot.add_cog(TreacheryAffixes(bot))
